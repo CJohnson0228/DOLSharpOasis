9af9894a120395c7125f466f720c6a5f0c989d44
Christopher M Johnson <c.meyer.johnson0228@outlook.com>
2026-01-07 21:03:59 -0500

    remove obelisk turn to interaction

--- GameServer/gameobjects/CustomNPC/Teleporters/AlbCatacombsObelisk.cs
+++ GameServer/gameobjects/CustomNPC/Teleporters/AlbCatacombsObelisk.cs
@@ -53,6 +53,7 @@ namespace DOL.GS
         /// <returns></returns>
         public override bool Interact(GamePlayer player)
         {
+            // removed interaction statement causing obelisk to turn
             String intro = String.Format(
                 "Adventurer, the catacombs are a dangerous place, but I may be able to ease the burden of travel. {0} {1} {2} {3} {4} {5} {6} {7} {8}",
                 "Locations I can teleport your mortal body too are as follows.\n", 
