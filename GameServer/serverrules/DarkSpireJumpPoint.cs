8915c18dc6efc763d571a88565805d1de4709ab7
Christopher M Johnson <c.meyer.johnson0228@outlook.com>
2026-01-10 21:04:08 -0500

    Added Darkspire Handler and Instance Files. Attempting to master Instanced Dungeons.

---
+++ GameServer/serverrules/DarkSpireJumpPoint.cs
@@ -0,0 +1,37 @@
+﻿using DOL.Database;
+using DOL.GS.PacketHandler;
+using log4net;
+using System.Reflection;
+
+namespace DOL.GS.ServerRules
+{
+    public class DarkSpireJumpPoint : IJumpPointHandler
+    {
+        private static readonly ILog log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+        public bool IsAllowedToJump(ZonePoint targetPoint, GamePlayer player)
+        {
+            DarkSpireInstance instance = GetDarkSpireInstance(player);
+            
+            if (instance == null)
+            {
+                player.Out.SendMessage("Failed to create DarkSpire instance!", eChatType.CT_System, eChatLoc.CL_SystemWindow);
+                return false;
+            }
+
+            targetPoint.TargetRegion = instance.ID;
+            
+            return true;
+        }
+
+        private DarkSpireInstance GetDarkSpireInstance(GamePlayer player)
+        {
+            // Create new instance using Region 69 as template
+            DarkSpireInstance instance = (DarkSpireInstance)WorldMgr.CreateInstance(69, typeof(DarkSpireInstance));
+            
+            log.InfoFormat("Created DarkSpire instance {0} for player {1}", instance.ID, player.Name);
+            
+            return instance;
+        }
+    }
+}
---
+++ GameServer/world/Instance/DarkSpireInstance.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using DOL.GS;
+using DOL.Database;
+using DOL.GS.PacketHandler;
+using log4net;
+using System.Reflection;
+
+namespace DOL.GS
+{
+    public class DarkSpireInstance : BaseInstance
+    {
+        private static readonly ILog log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+        public DarkSpireInstance(ushort ID, GameTimer.TimeManager time, RegionData data)
+            : base(ID, time, data)
+        {
+            DestroyWhenEmpty = true;
+        }
+
+        public override void LoadFromDatabase(Mob[] mobObjs, ref long mobCount, ref long merchantCount, ref long itemCount, ref long bindCount)
+        {
+            base.LoadFromDatabase(mobObjs, ref mobCount, ref merchantCount, ref itemCount, ref bindCount);
+            
+            foreach (GameNPC mob in GetMobsInsideInstance(false))
+            {
+                if (mob != null)
+                {
+                    mob.RespawnInterval = -1;
+                }
+            }
+            
+            log.InfoFormat("DarkSpire instance {0} loaded with {1} mobs (no respawn)", ID, mobCount);
+        }
+
+        public override void OnPlayerEnterInstance(GamePlayer player)
+        {
+            base.OnPlayerEnterInstance(player);
+            player.Out.SendMessage("You have entered the DarkSpire!", eChatType.CT_System, eChatLoc.CL_SystemWindow);
+        }
+    }
+}
